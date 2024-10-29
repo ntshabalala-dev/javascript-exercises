9408149ac57ef87361876af825bba8ec69f416d5
sonofglassperson <ntshabalalait@gmail.com>
2024-10-29 19:17:39 +0200

    02-repeat-string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let repeat = '';
+    if (num < 0) {
+        return 'ERROR';
+    }
+    for (let index = 0; index < num; index++) {
+        repeat += string;
+    }
+    return repeat;
 };
 
 // Do not edit below this line
