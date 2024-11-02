41d2edc0b6ea4359700194ef4c677bf6cf60062d
sonofglassperson <ntshabalalait@gmail.com>
2024-11-02 23:11:44 +0200

    05_sumall

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let sumAll = 0
+    for (let index = start; index <= end; index++) {
+        sumAll += index
+    }
+    return sumAll;
 };
 
 // Do not edit below this line
