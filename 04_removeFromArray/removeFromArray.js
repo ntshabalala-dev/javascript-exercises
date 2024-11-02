ffc283d611add7a718bd121f96e7b4aa433dbe6a
sonofglassperson <ntshabalalait@gmail.com>
2024-11-02 20:34:50 +0200

    04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...numsToRemove) {
+    let copyArray = [...array]
+    emptyArray = copyArray.filter(
+        // does not include [1, 2].includes(element)
+        (element) => ! numsToRemove.includes(element)
+    );
+    // copyArray.forEach(element => {
+    //     index = numsToRemove.includes(element);
+    //     if (!index) {
+    //         console.log(element);
+    //         emptyArray.push(element)
+    //     }
+    // });
+    return emptyArray
 };
 
-// Do not edit below this line
+// // Do not edit below this line
 module.exports = removeFromArray;
