dc35be7feee699e7874aac560c29d34030381396
sonofglassperson <ntshabalalait@gmail.com>
2024-11-02 11:13:08 +0200

    03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function(text) {
+    let newArray = []
+    let stringArray = text.split('');
+    let stringArrayLength = stringArray.length;
 
+    for (let index = 0; index < stringArray.length; index++) {
+        stringArrayLength -= 1    
+        newArray[index] = stringArray[stringArrayLength]  
+    }
+
+    return newArray.join('');
 };
 
 // Do not edit below this line
