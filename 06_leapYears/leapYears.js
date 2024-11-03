da9240cfdf9d12b7835e9d54d0389b3d61f6144b
sonofglassperson <ntshabalalait@gmail.com>
2024-11-03 09:06:41 +0200

    06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,15 @@
-const leapYears = function() {
-
+const leapYears = function(year = 0) {
+let isLeapYear = false;
+    if (year % 4  == 0) {
+        isLeapYear = true;;
+    }
+    if (year % 100 == 0 ) {
+        isLeapYear = false
+        if (year % 400 == 0) {
+            isLeapYear = true;
+        }
+    }    
+    return Number.isInteger(year) ? isLeapYear : false;
 };
-
 // Do not edit below this line
 module.exports = leapYears;
