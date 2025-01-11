749333de8d622dff536ca255e634ba68f06802d6
ntshabalala-dev <ntshabalalait@gmail.com>
2025-01-11 22:24:37 +0200

    09_palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (text) {
+    text = text.replaceAll(' ', '').toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
 
+    let palindrome = '';
+    for (let index = text.length - 1; index >= 0; index--) {
+        palindrome += text[index];
+    }
+    return palindrome == text;
 };
 
 // Do not edit below this line
