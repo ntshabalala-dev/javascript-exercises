52381f6352979ce4789ab07bd102df3cebf12940
ntshabalala-dev <ntshabalalait@gmail.com>
2025-01-12 15:30:14 +0200

    10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function (b) {
+    let a = []
+    let j = 1;
 
+    if (b < 0) {
+        return 'OOPS'
+    }
+
+    for (let index = 0; index < b; index++) {
+        if (index == 0 || index == 1) {
+            a.push(j)
+        } else {
+            a.push(a[index - 2] + a[index - 1])
+        }
+    }
+    a.unshift(0)
+    return a[a.length - 1]
 };
 
 // Do not edit below this line
