65be3c4d358817b38475a8d7f2513b40b648b849
ntshabalala-dev <ntshabalalait@gmail.com>
2025-01-13 15:26:57 +0200

    12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+    let p = people.sort((a, b) => {
+        const currentYear = new Date().getFullYear();
+        let aOldestYear = a.yearOfDeath
+        let bOldestYear = b.yearOfDeath
 
+        if ('yearOfDeath' in b == false) {
+            bOldestYear = currentYear
+        }
+        if ('yearOfDeath' in a == false) {
+            aOldestYear = currentYear
+        }
+        return (aOldestYear - a.yearOfBirth) - (bOldestYear - b.yearOfBirth)
+    })
+    return p[p.length - 1];
 };
 
 // Do not edit below this line
